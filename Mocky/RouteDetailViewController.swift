104c3cd8dcd7163ba7263ea17b67fa5776fc9b3d
Nick Hayward <nehayward@gmail.com>
2019-06-21 13:32:04 -0700

    Add tableview

---
+++ Mocky/RouteDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RouteDetailViewController.swift
+//  Mocky
+//
+//  Created by Nick Hayward on 6/21/19.
+//  Copyright © 2019 Nick Hayward. All rights reserved.
+//
+
+import Cocoa
+
+class RouteDetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Mocky/SplitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SplitViewController.swift
+//  Mocky
+//
+//  Created by Nick Hayward on 6/21/19.
+//  Copyright © 2019 Nick Hayward. All rights reserved.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        
+//        let item = NSSplitViewItem(viewController: RoutesTableViewController())
+//        self.addSplitViewItem(item)
+    }
+    
+}
